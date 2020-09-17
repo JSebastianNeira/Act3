af6f599c2876d394d39a894eb92e58996130c124
Sebastián Neira <a01570411@itesm.mx>
2020-09-17 14:19:19 -0500

    Fantasmas Inteligentes
    Utilize una varible que guarda la posición de los fantasmas y se utiliza esa variable para verificar si ahi hay un punto.
    
    Si no hay punto es como si chocara contra una pared, lo que hace que los fantaasmas solo se queden en los puntos haciendo asi que le sea imposible a Pacman ganar

--- act3.py
+++ act3.py
@@ -1,17 +1,19 @@
+# Juan Sebastián Neira González
+# Adán Daniel Márquez Hernández a00827172
 from random import choice
 from turtle import *
 from freegames import floor, vector
 
-state = {'score': 0}
-path = Turtle(visible=False)
+state = {'score': 0} #Variable que guarda el marcador cada vez que Pacman pasa sobre un punto blanco
+path = Turtle(visible=False) 
 writer = Turtle(visible=False)
 aim = vector(5, 0)
-pacman = vector(-40, -80)
+pacman = vector(-40, -80) #Vector que guarda la posicion inicial de Pacman
 ghosts = [
-    [vector(-180, 160), vector(7, 0),],
-    [vector(-180, -160), vector(0, 7)],
-    [vector(100, 160), vector(0, -7)],
-    [vector(100, -160), vector(-7, 0)],
+    [vector(-180, 160), vector(10, 0)], #Vector que guarda las posiciones iniciales de los Fantasmas junto con la direccion que van a tomar.
+    [vector(-180, -160), vector(0, 10)],
+    [vector(100, 160), vector(0, -10)],
+    [vector(100, -160), vector(-10, 0)],
 ]
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -34,9 +36,9 @@ tiles = [
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-]
+] #Tablero donde los 0 son paredes y los 1 son los caminos
 
-def square(x, y):
+def square(x, y): #Función que dibuja el cuadrado cada vez que pacman pasa sobre un punto
     "Draw square using path at (x, y)."
     path.up()
     path.goto(x, y)
@@ -49,14 +51,14 @@ def square(x, y):
 
     path.end_fill()
 
-def offset(point):
+def offset(point): #Regresa la posición del tablero dadas las coordenadas del vector
     "Return offset of point in tiles."
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
 
-def valid(point):
+def valid(point): #Valida que la posición del tablero sea un 1 y no un 0 (Para que no corra sobre las paredes)
     "Return True if point is valid in tiles."
     index = offset(point)
 
@@ -70,7 +72,7 @@ def valid(point):
 
     return point.x % 20 == 0 or point.y % 20 == 0
 
-def world():
+def world(): #Dibuja el tablero completo
     "Draw world using path."
     bgcolor('black')
     path.color('blue')
@@ -88,7 +90,7 @@ def world():
                 path.goto(x + 10, y + 10)
                 path.dot(2, 'white')
 
-def move():
+def move(): #Mueve a Pacman y a los fantasmas
     "Move pacman and all ghosts."
     writer.undo()
     writer.write(state['score'])
@@ -96,12 +98,12 @@ def move():
     clear()
 
     if valid(pacman + aim):
-        pacman.move(aim)
+        pacman.move(aim) #Valida que cada vez que le piques a una flehca para cambiar de dirrección se pueda pasar por ahi
 
     index = offset(pacman)
 
-    if tiles[index] == 1:
-        tiles[index] = 2
+    if tiles[index] == 1: #Si la tile del tablero es 1 significa que hay un punto lo cual aumenta el score
+        tiles[index] = 2 #Cuando pasa por ese punto lo pone como 2 para que ya no haya punto
         state['score'] += 1
         x = (index % 20) * 20 - 200
         y = 180 - (index // 20) * 20
@@ -111,15 +113,17 @@ def move():
     goto(pacman.x + 10, pacman.y + 10)
     dot(20, 'yellow')
 
-    for point, course in ghosts:
-        if valid(point + course):
-            point.move(course)
-        else:
+    for point, course in ghosts: #Por cada posición y curso de los fantasmas los mueve respectivamente
+        index = offset(point + course) #Guarda la posición en el tablero de cada fantasma
+        if valid(point + course) and tiles[index] == 1: #Valida que el la casilla a la que se mueva sea valida (>0) y que tenga un punto(1)
+            #Esto hace que los fantasmas sean más inteligentes y hace que Pacman nunca pueda ganar
+            point.move(course) 
+        else: #Si no cumple ninguna de las condiciones anteriores busca una opción de dirección a moverse
             options = [
-                vector(7, 0),
-                vector(-7, 0),
-                vector(0, 7),
-                vector(0, -7),
+                vector(10, 0),
+                vector(-10, 0),
+                vector(0, 10),
+                vector(0, -10),
             ]
             plan = choice(options)
             course.x = plan.x
@@ -129,15 +133,15 @@ def move():
         goto(point.x + 10, point.y + 10)
         dot(20, 'red')
 
-    update()
+        update()
 
-    for point, course in ghosts:
+    for point, course in ghosts:  #Confirma que Pacman y los fantasmas sno esten en la misma casilla
         if abs(pacman - point) < 20:
             return
 
     ontimer(move, 100)
 
-def change(x, y):
+def change(x, y): #Cambia la dirreción de pacman cada vez que se le pica a una flecha
     "Change pacman aim if valid."
     if valid(pacman + vector(x, y)):
         aim.x = x
